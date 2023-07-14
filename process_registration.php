3d4fde62b25bbc55b842e483d5f6296edad5959c
ShahMann <mann.shah@brainvire.com>
2023-07-14 15:34:10 +0530

    Coutry code added in database

--- process_registration.php
+++ process_registration.php
@@ -4,11 +4,8 @@
  if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
  }
- else{
-  echo "connected";
- }
+ 
 if (isset($_POST["submit"])) {
-  // print_r($_POST);
   $firstName = $_POST['firstName'];
   $lastName = $_POST['lastName'];
   $email = $_POST['email'];
@@ -17,20 +14,58 @@ if (isset($_POST["submit"])) {
   $confirmPassword = $_POST['confirmPassword'];
   $mobileNumber = $_POST['mobileNumber'];
   $country = $_POST['country'];
-
-  
   $countryId = $_POST['country'];
-  print_r($countryId);
-  // die();
+
+  $errors = [];
+
+  if (empty($firstName)) {
+    $errors[] = "First name is required";
+  }
+
+  if (empty($lastName)) {
+    $errors[] = "Last name is required";
+  }
+
+  if (empty($email)) {
+    $errors[] = "Email is required";
+  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = "Invalid email format";
+  }
+
+  if (empty($username)) {
+    $errors[] = "Username is required";
+  }
+
+  if (empty($password)) {
+    $errors[] = "Password is required";
+  }
+
+  if ($password !== $confirmPassword) {
+    $errors[] = "Passwords do not match";
+  }
+
+  if (empty($mobileNumber)) {
+    $errors[] = "Mobile number is required";
+  }
+
+  if (empty($country)) {
+    $errors[] = "Country is required";
+  }
+
+  if (empty($errors)) {
+    $conn = mysqli_connect("localhost", "root", "root", "demo");
 
     $query = "INSERT INTO users (first_name, last_name, email, username, password, mobile_number, country) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "ssssssi", $firstName, $lastName, $email, $username, $password, $mobileNumber, $countryId);
+    mysqli_stmt_bind_param($stmt, "sssssss", $firstName, $lastName, $email, $username, $password, $mobileNumber, $countryId);
     mysqli_stmt_execute($stmt);
 
-    echo "Registration successful!";
-  } else {
-    echo implode("\n", $errors);
-  }
+    echo "<script>alert('Registration successful!'); window.location.href = '1.php'; </script>";
 
+  } else {
+    // echo implode("\n", $errors);
+    echo "<script>alert('" . implode("\\n", $errors) . "'); window.location.href = '1.php';</script>";
+
+  }
+}
 ?>
